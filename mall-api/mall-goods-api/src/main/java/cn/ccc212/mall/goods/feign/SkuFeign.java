e766f7fbab383b18c8488d9a69c8f80d63a3d1a4
Unknown <2120496201@qq.com>
2025-12-29 21:44:36 +0800

    feat(feign): 新增 OpenFeign 客户端调用

---
+++ mall-api/mall-goods-api/src/main/java/cn/ccc212/mall/goods/feign/SkuFeign.java
@@ -0,0 +1,21 @@
+package cn.ccc212.mall.goods.feign;
+
+import cn.ccc212.mall.util.RespResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "mall-goods", path = "/sku")
+public interface SkuFeign {
+    
+    @GetMapping("/aditems/type")
+    RespResult<?> typeitems(@RequestParam Integer id);
+
+    @DeleteMapping(value = "/deleteAditems/type")
+    RespResult<?> deleteTypeItems(@RequestParam Integer id);
+
+    @PutMapping(value = "/updateAitems/type")
+    RespResult<?> updateTypeItems(@RequestParam Integer id);
+}
