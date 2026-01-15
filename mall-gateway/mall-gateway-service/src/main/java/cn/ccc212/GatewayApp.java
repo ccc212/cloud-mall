dc3ba9ba08fb1d8ca34d53c13f6eac5eb8af85d7
叶嘉谦 <yejiaqian@qq.com>
2026-01-15 23:35:52 +0800

    feat(gateway):新增网关访问，使用8087端口和配置服务名前缀访问

---
+++ mall-gateway/mall-gateway-service/src/main/java/cn/ccc212/GatewayApp.java
@@ -0,0 +1,20 @@
+package cn.ccc212;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+public class GatewayApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(GatewayApp.class, args);
+    }
+}
